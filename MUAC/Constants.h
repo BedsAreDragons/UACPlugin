1f196729fb3dfb68e613ea9790b78c5e67358009
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-09-24 18:32:11 +0100

    Update Constants.h

--- MUAC/Constants.h
+++ MUAC/Constants.h
@@ -7,7 +7,7 @@ using namespace EuroScopePlugIn;
 
 const string PLUGIN_NAME = "MUAC PlugIn";
 const string PLUGIN_VERSION = "@appveyor_build";
-const string PLUGIN_AUTHOR = "github.com/pierr3";
+const string PLUGIN_AUTHOR = "vats.im/uac";
 const string PLUGIN_COPY = "GPL v3";
 
 #define MUAC_RADAR_SCREEN_VIEW "MUAC Radar Screen"
