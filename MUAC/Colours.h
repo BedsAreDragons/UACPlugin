5bf73917c257b16c6ac0ba3e4c407a5c00fa3d58
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-10-01 18:37:13 +0100

    Update Colours.h

--- MUAC/Colours.h
+++ MUAC/Colours.h
@@ -6,7 +6,7 @@ using namespace Gdiplus;
 namespace Colours {
 	const Color AircraftGreen(23, 185, 27); // 28, 190, 32
 	const Color AircraftBlue(111, 177, 203);
-	const Color AircraftLightGrey(165, 165, 165);
+	const Color AircraftLightGrey(255, 255, 255);
 	const Color AircraftDarkGrey(79, 79, 79);
 	const Color YellowWarning(167, 167, 0);
 	const Color PurpleDisplay(235, 0, 201);
@@ -18,8 +18,8 @@ namespace Colours {
 
 	const Color YellowHighlight(255, 255, 0);
 	
-	const Color DarkBlueMenu(0, 2, 48);
-	const Color LightBlueMenu(23, 156, 155);
+	const Color DarkBlueMenu(132, 136, 145);
+	const Color LightBlueMenu(255, 255, 255);
 	const Color MenuButtonTop(165, 165, 165);
 	const Color MenuButtonBottom(74, 74, 74);
 	const Color GreyTextMenu(162, 162, 162);
