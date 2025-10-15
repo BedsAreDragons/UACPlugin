e1ca239a6a712ce75016aa45dcebfea1308711d0
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-10-15 17:57:21 +0100

    Update resource.h

--- MUAC/resource.h
+++ MUAC/resource.h
@@ -2,6 +2,37 @@
 // Microsoft Visual C++ generated include file.
 // Used by MUAC.rc
 
+
+#define IDC_POINTER                     104
+#define IDC_SMRCURSOR                   104
+#define IDC_POINTER_COPY                105
+#define IDD_DIALOG1                     7000
+#define IDD_DIALOG2                     7001
+#define IDR_WAVE1                       7002
+#define IDC_SMRMOVECURSOR               7003
+#define IDC_CALLSIGN                    7004
+#define IDC_FROM                        7005
+#define IDC_DEST                        7006
+#define IDC_SMRMOVETAG                  7006
+#define IDC_SID                         7007
+#define IDC_CTOT                        7008
+#define IDC_SMRCORRELATE                7008
+#define IDC_TSAT                        7010
+#define IDC_SMRMOVE                     7010
+#define IDC_SMRMOVEWINDOW               7010
+#define IDC_MESSAGE                     7012
+#define IDC_ACT                         7013
+#define IDC_RWY                         7014
+#define IDC_SMRRESIZE                   7014
+#define IDC_CLB                         7015
+#define IDC_SSR                         7016
+#define IDC_FREQ                        7017
+#define IDC_LOGON                       7017
+#define IDC_PASSWORD                    7018
+#define IDC_SOUND                       7019
+#define IDC_ORIG                        7020
+
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
