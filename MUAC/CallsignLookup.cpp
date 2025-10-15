e2a377b7773f893fe15de05faaa9ba107e7e3091
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-10-15 17:39:39 +0100

    Update CallsignLookup.cpp

--- MUAC/CallsignLookup.cpp
+++ MUAC/CallsignLookup.cpp
@@ -5,7 +5,7 @@
 // CCallsignLookup Class by Even Rognlien, used with permission
 //
 
-bool CCallsignLookup::Available = false;
+bool CCallsignLookup::Available = true;
 CCallsignLookup* CCallsignLookup::Lookup = nullptr;
 
 CCallsignLookup::CCallsignLookup(std::string fileName) {
