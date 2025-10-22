a0922e11033781ce13616c5da4d5b30ca5db9079
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-10-22 17:16:28 +0100

    Update HttpHelper.cpp

--- MUAC/HttpHelper.cpp
+++ MUAC/HttpHelper.cpp
@@ -1,45 +1,27 @@
 #include "stdafx.h"
 #include "HttpHelper.h"
 
-//
-// HttpHelper Class by Even Rognlien, used with permission
-//
+string LoadHttpString(string url)
+{
+	const string AGENT{ "EuroScopeMUAC/" + PLUGIN_VERSION };
+	HINTERNET connect = InternetOpen(AGENT.c_str(), INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+	if (connect) {
+		HINTERNET OpenAddress = InternetOpenUrl(connect, url.c_str(), NULL, 0, INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_RELOAD, 0);
+		if (OpenAddress) {
+			char DataReceived[256];
+			DWORD NumberOfBytesRead{ 0 };
+			std::string answer;
+			while (InternetReadFile(OpenAddress, DataReceived, 256, &NumberOfBytesRead) && NumberOfBytesRead)
+				answer.append(DataReceived, NumberOfBytesRead);
 
-std::string HttpHelper::downloadedContents;
-
-HttpHelper::HttpHelper()  {
-
-}
-
-// Used for downloading strings from web:
-size_t HttpHelper::handle_data(void *ptr, size_t size, size_t nmemb, void *stream) {
-	int numbytes = size*nmemb;
-	// The data is not null-terminated, so get the last character, and replace it with '\0'. 
-	char lastchar = *((char *)ptr + numbytes - 1);
-	*((char *)ptr + numbytes - 1) = '\0';
-	downloadedContents.append((char *)ptr);
-	downloadedContents.append(1, lastchar);
-	*((char *)ptr + numbytes - 1) = lastchar;  // Might not be necessary. 
-	return size*nmemb;
-}
-
-std::string HttpHelper::downloadStringFromURL(std::string url) {
-	CURL *curl = curl_easy_init();
-	if (curl)
-	{
-		downloadedContents = "";
-		// Tell libcurl the URL 
-		curl_easy_setopt(curl, CURLOPT_URL, url);
-		// Tell libcurl what function to call when it has data 
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, HttpHelper::handle_data);
-		// Do it! 
-		CURLcode res = curl_easy_perform(curl);
-		curl_easy_cleanup(curl);
-		return downloadedContents;
+			InternetCloseHandle(OpenAddress);
+			InternetCloseHandle(connect);
+			return answer;
+		}
+		else {
+			InternetCloseHandle(connect);
+		}
 	}
+
 	return "";
-}
-
-HttpHelper::~HttpHelper() {
-
 }
