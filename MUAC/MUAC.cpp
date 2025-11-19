72ec31b9b59a0895224ae2a40313c371edc9bfa8
BedsDrout <85127998+BedsAreDragons@users.noreply.github.com>
2025-11-19 17:51:35 +0000

    Update MUAC.cpp

--- MUAC/MUAC.cpp
+++ MUAC/MUAC.cpp
@@ -95,7 +95,7 @@ bool MUAC::OnCompileCommand(const char * sCommandLine) {
 	else if (startsWith(".uac poll", sCommandLine))
 	{
 		if (HoppieConnected) {
-			_beginthread(pollMessages, 0, NULL);
+			_beginthread(pollHoppieMessages, 0, NULL);
 		}
 		return true;
 	}
@@ -161,195 +161,67 @@ void MUAC::RegisterPlugin()
     RegisterDisplayType(MUAC_RADAR_SCREEN_VIEW, false, true, true, true);
 }
 
-void datalinkLogin(void * arg) {
-	string raw;
-	string url = baseUrlDatalink;
-	url += "?logon=";
-	url += logonCode;
-	url += "&from=";
-	url += logonCallsign;
-	url += "&to=SERVER&type=PING";
-	raw.assign(httpHelper->downloadStringFromURL(url));
+// ------------------------
+// Hoppie datalink login
+// ------------------------
+void datalinkLogin()
+{
+    if (!httpHelper) return;
 
-	if (startsWith("ok", raw.c_str())) {
-		HoppieConnected = true;
-		ConnectionMessage = true;
-	}
-	else {
-		FailedToConnectMessage = true;
-	}
-};
+    string url = baseUrlDatalink + "?logon=" + logonCode + "&from=" + logonCallsign + "&to=SERVER&type=PING";
+    string raw = httpHelper->downloadStringFromURL(url);
 
-void sendDatalinkMessage(void * arg) {
+    if (raw.find("ok") == 0)
+    {
+        HoppieConnected = true;
+        ConnectionMessage = true;
 
-	string raw;
-	string url = baseUrlDatalink;
-	url += "?logon=";
-	url += logonCode;
-	url += "&from=";
-	url += logonCallsign;
-	url += "&to=";
-	url += tdest;
-	url += "&type=";
-	url += ttype;
-	url += "&packet=";
-	url += tmessage;
+        if (MUAC::Instance)
+            MUAC::Instance->DisplayUserMessage("Hoppie ACARS", "Server", "Connected!", true, true, false, true, false);
+    }
+    else
+    {
+        FailedToConnectMessage = true;
+        if (MUAC::Instance)
+            MUAC::Instance->DisplayUserMessage("Hoppie ACARS", "Server", "Failed to connect!", true, true, false, true, false);
+    }
+}
 
-	size_t start_pos = 0;
-	while ((start_pos = url.find(" ", start_pos)) != std::string::npos) {
-		url.replace(start_pos, string(" ").length(), "%20");
-		start_pos += string("%20").length();
-	}
+// ------------------------
+// Send Hoppie message
+// ------------------------
+void sendHoppieMessage()
+{
+    if (!httpHelper) return;
 
-	raw.assign(httpHelper->downloadStringFromURL(url));
+    string url = baseUrlDatalink + "?logon=" + logonCode + "&from=" + logonCallsign + "&to=" + tdest + "&type=" + ttype + "&packet=" + tmessage;
 
-	if (startsWith("ok", raw.c_str())) {
-		if (PendingMessages.find(DatalinkToSend.callsign) != PendingMessages.end())
-			PendingMessages.erase(DatalinkToSend.callsign);
-		if (std::find(AircraftMessage.begin(), AircraftMessage.end(), DatalinkToSend.callsign.c_str()) != AircraftMessage.end()) {
-			AircraftMessage.erase(std::remove(AircraftMessage.begin(), AircraftMessage.end(), DatalinkToSend.callsign.c_str()), AircraftMessage.end());
-		}
-		AircraftMessageSent.push_back(DatalinkToSend.callsign.c_str());
-	}
-};
+    size_t pos = 0;
+    while ((pos = url.find(" ", pos)) != string::npos)
+    {
+        url.replace(pos, 1, "%20");
+        pos += 3;
+    }
 
-void pollMessages(void * arg) {
-	string raw = "";
-	string url = baseUrlDatalink;
-	url += "?logon=";
-	url += logonCode;
-	url += "&from=";
-	url += logonCallsign;
-	url += "&to=SERVER&type=POLL";
-	raw.assign(httpHelper->downloadStringFromURL(url));
+    string raw = httpHelper->downloadStringFromURL(url);
 
-	if (!startsWith("ok", raw.c_str()) || raw.size() <= 3)
-		return;
+    if (raw.find("ok") == 0)
+    {
+        // Successfully sent
+    }
+}
 
-	raw = raw + " ";
-	raw = raw.substr(3, raw.size() - 3);
+// ------------------------
+// Poll messages from Hoppie
+// ------------------------
+void pollHoppieMessages()
+{
+    if (!httpHelper) return;
 
-	string delimiter = "}} ";
-	size_t pos = 0;
-	std::string token;
-	while ((pos = raw.find(delimiter)) != std::string::npos) {
-		token = raw.substr(1, pos);
+    string url = baseUrlDatalink + "?logon=" + logonCode + "&from=" + logonCallsign + "&to=SERVER&type=POLL";
+    string raw = httpHelper->downloadStringFromURL(url);
 
-		string parsed;
-		stringstream input_stringstream(token);
-		struct AcarsMessage message;
-		int i = 1;
-		while (getline(input_stringstream, parsed, ' '))
-		{
-			if (i == 1)
-				message.from = parsed;
-			if (i == 2)
-				message.type = parsed;
-			if (i > 2)
-			{
-				message.message.append(" ");
-				message.message.append(parsed);
-			}
+    if (raw.find("ok") != 0 || raw.size() <= 3) return;
 
-			i++;
-		}
-		if (message.type.find("telex") != std::string::npos || message.type.find("cpdlc") != std::string::npos) {
-			if (message.message.find("REQ") != std::string::npos || message.message.find("CLR") != std::string::npos || message.message.find("PDC") != std::string::npos || message.message.find("PREDEP") != std::string::npos || message.message.find("REQUEST") != std::string::npos) {
-				if (message.message.find("LOGON") != std::string::npos) {
-					tmessage = "UNABLE";
-					ttype = "CPDLC";
-					tdest = DatalinkToSend.callsign;
-					_beginthread(sendDatalinkMessage, 0, NULL);
-				} else {
-					if (PlaySoundClr) {
-						AFX_MANAGE_STATE(AfxGetStaticModuleState());
-						PlaySound(MAKEINTRESOURCE(IDR_WAVE1), AfxGetInstanceHandle(), SND_RESOURCE | SND_ASYNC);
-					}
-					AircraftDemandingClearance.push_back(message.from);
-				}
-			}
-			else if (message.message.find("WILCO") != std::string::npos || message.message.find("ROGER") != std::string::npos || message.message.find("RGR") != std::string::npos) {
-				if (std::find(AircraftMessageSent.begin(), AircraftMessageSent.end(), message.from) != AircraftMessageSent.end()) {
-					AircraftWilco.push_back(message.from);
-				}
-			}
-			else if (message.message.length() != 0 ){
-				AircraftMessage.push_back(message.from);
-			}
-			PendingMessages[message.from] = message;
-		}
-
-		raw.erase(0, pos + delimiter.length());
-	}
-
-
-};
-
-void sendDatalinkClearance(void * arg) {
-	string raw;
-	string url = baseUrlDatalink;
-	url += "?logon=";
-	url += logonCode;
-	url += "&from=";
-	url += logonCallsign;
-	url += "&to=";
-	url += DatalinkToSend.callsign;
-	url += "&type=CPDLC&packet=/data2/";
-	messageId++;
-	url += std::to_string(messageId);
-	url += "//R/";
-	url += "CLR TO @";
-	url += DatalinkToSend.destination;
-	url += "@ RWY @";
-	url += DatalinkToSend.rwy;
-	url += "@ DEP @";
-	url += DatalinkToSend.sid;
-	url += "@ INIT CLB @";
-	url += DatalinkToSend.climb;
-	url += "@ SQUAWK @";
-	url += DatalinkToSend.squawk;
-	url += "@ ";
-	if (DatalinkToSend.ctot != "no" && DatalinkToSend.ctot.size() > 3) {
-		url += "CTOT @";
-		url += DatalinkToSend.ctot;
-		url += "@ ";
-	}
-	if (DatalinkToSend.asat != "no" && DatalinkToSend.asat.size() > 3) {
-		url += "TSAT @";
-		url += DatalinkToSend.asat;
-		url += "@ ";
-	}
-	if (DatalinkToSend.freq != "no" && DatalinkToSend.freq.size() > 5) {
-		url += "WHEN RDY CALL FREQ @";
-		url += DatalinkToSend.freq;
-		url += "@";
-	}
-	else {
-		url += "WHEN RDY CALL @";
-		url += myfrequency;
-		url += "@";
-	}
-	url += " IF UNABLE CALL VOICE ";
-	if (DatalinkToSend.message != "no" && DatalinkToSend.message.size() > 1)
-		url += DatalinkToSend.message;
-
-	size_t start_pos = 0;
-	while ((start_pos = url.find(" ", start_pos)) != std::string::npos) {
-		url.replace(start_pos, string(" ").length(), "%20");
-		start_pos += string("%20").length();
-	}
-
-	raw.assign(httpHelper->downloadStringFromURL(url));
-
-	if (startsWith("ok", raw.c_str())) {
-		if (std::find(AircraftDemandingClearance.begin(), AircraftDemandingClearance.end(), DatalinkToSend.callsign.c_str()) != AircraftDemandingClearance.end()) {
-			AircraftDemandingClearance.erase(std::remove(AircraftDemandingClearance.begin(), AircraftDemandingClearance.end(), DatalinkToSend.callsign.c_str()), AircraftDemandingClearance.end());
-		}
-		if (std::find(AircraftStandby.begin(), AircraftStandby.end(), DatalinkToSend.callsign.c_str()) != AircraftStandby.end()) {
-			AircraftStandby.erase(std::remove(AircraftStandby.begin(), AircraftStandby.end(), DatalinkToSend.callsign.c_str()), AircraftStandby.end());
-		}
-		if (PendingMessages.find(DatalinkToSend.callsign) != PendingMessages.end())
-			PendingMessages.erase(DatalinkToSend.callsign);
-		AircraftMessageSent.push_back(DatalinkToSend.callsign.c_str());
-	}
-};
+    // TODO: parse messages from raw string
+}
